12de9cb358dd09bdbcd055dd814b90133bcd9ca6
radium <r@di.um>
2019-09-17 11:57:23 +0200

    Clean some stuff

--- modules/forge/src/main/scala/com/github/radium226/forge/run/Runner.scala
+++ modules/forge/src/main/scala/com/github/radium226/forge/run/Runner.scala
@@ -9,15 +9,17 @@ import cats.implicits._
 
 class Runner[F[_]](folderPath: Path, kinds: List[Kind]) {
 
-  def run(phase: Phase)(implicit F: Sync[F]): F[Unit] = {
-    kinds.traverse(_.run(folderPath).applyOrElse(phase, F.unit)).void
+  def run(phase: Phase)(implicit F: Concurrent[F]): F[Unit] = {
+    kinds.traverse({ kind =>
+      kind.run(folderPath).applyOrElse(phase, F.unit)
+    }).void
   }
 
 }
 
 object Runner {
 
-  def apply[F[_]](folderPath: Path)(implicit F: Sync[F]): F[Runner[F]] = {
+  def apply[F[_]](folderPath: Path)(implicit F: Concurrent[F]): F[Runner[F]] = {
     F.pure(new Runner[F](folderPath, List(Make, Sbt)))
   }
 
